afa695a2b53173e3f7bd122816bdf425947ebc1c
SerenaIng <35091754+SerenaIng@users.noreply.github.com>
2019-03-19 01:18:05 -0700

    update
    player mvmt &  srdtfygiuhlij;ok'l

--- Player.gd
+++ Player.gd
@@ -1,52 +1,26 @@
 extends KinematicBody2D
 
-const MOVE_SPEED = 500
-const JUMP_FORCE = 2000
-const GRAVITY = 50
-const MAX_FALL_SPEED = 1000
+var velocity = Vector2(0,0)
+export var SPEED = 300
 
-onready var anim_player = $AnimationPlayer
-onready var sprite = $Sprite
+func _ready():
+    # Called when the node is added to the scene for the first time.
+    # Initialization here
+    pass
 
-var y_velo = 0
-var facing_right = false
+func _process(delta):
+    if(Input.is_action_pressed("move_left")):
+        velocity.x = -SPEED
+        $AnimatedSprite.flip_h = true
+        $AnimatedSprite.play("walk")
+    elif(Input.is_action_pressed("move_right")):
+        velocity.x = SPEED
+        $AnimatedSprite.flip_h = false
+        $AnimatedSprite.play("walk")
+    else:
+        velocity.x = 0
+        $AnimatedSprite.play("idle")
 
-func _physics_process(delta):
-	var move_dir = 0
-	if Input.is_action_pressed("move_right"):
-		move_dir += 1
-	if Input.is_action_pressed("move_left"):
-		move_dir -= 1
-	
-	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
-	
-	var grounded = is_on_floor()
-	y_velo += GRAVITY
-	if grounded and Input.is_action_just_pressed("jump"):
-		y_velo = -JUMP_FORCE
-	if grounded and y_velo >= 0:
-		y_velo = 5
-	if y_velo > MAX_FALL_SPEED:
-		y_velo = MAX_FALL_SPEED
-	
-	if facing_right and move_dir < 0:
-		flip()
-	if !facing_right and move_dir > 0:
-		flip()
-	
-	if grounded:
-		if move_dir == 0:
-			play_anim("idle")
-		else:
-			play_anim("walk")
-	else:
-		play_anim("jump")
+    move_and_slide(velocity)
 
-func flip():
-	facing_right = !facing_right
-	sprite.flip_h = !sprite.flip_h
-
-func play_anim(anim_name):
-	if anim_player.is_playing() and anim_player.current_animation == anim_name:
-		return
-	anim_player.play(anim_name)
+    pass
