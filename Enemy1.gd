9b8156bfdb555166ff3fab2c07d402da625e0395
SerenaIng <35091754+SerenaIng@users.noreply.github.com>
2019-03-25 22:29:08 -0700

    update
    update world

---
+++ Enemy1.gd
@@ -0,0 +1,77 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+var SPEED = 100
+var integer = 0
+var rando = 0
+var ydir = 100
+#export var SPEED = 300
+export var gravity = 400
+export var jumpV = -400
+
+func _ready():
+    # Called when the node is added to the scene for the first time.
+    # Initialization here
+	velocity.x = -SPEED
+	pass
+	
+func _process(delta):
+	if (velocity.y <=0):
+		if(rando == 0):
+    	    velocity.x = -SPEED
+    	    $AnimatedSprite.flip_h = true
+    	    $AnimatedSprite.play("walk")
+		else: 
+    	    velocity.x = SPEED
+    	    $AnimatedSprite.flip_h = false
+    	    $AnimatedSprite.play("walk")
+	else:
+		velocity.x = 0
+	move_and_slide(velocity,Vector2(0,0))
+
+#func _on_JumpBox1_area_entered(area):
+#	if area.is_in_group("jump"):
+#		velocity.y = jumpV
+#		set_collision_mask_bit(2, false)
+#	if area.is_in_group("floor"):
+#		velocity.y = 0
+#		set_collision_mask_bit(2, true)
+
+
+#func _on_DownBox1_area_entered(area):
+#	if area.is_in_group("down"):
+#		velocity.y = gravity
+#		set_collision_mask_bit(2, false)
+
+
+#func _on_JumpBox1_area_exited(area):
+#	if area.is_in_group("floor"):
+#		velocity.y = gravity
+#		set_collision_mask_bit(2, false)
+#	rando = randi()%2
+
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("down"):
+		velocity.y = gravity
+		set_collision_mask_bit(2, false)
+		pass # replace with function body
+
+
+func _on_JumpBox_area_entered(area):
+	if area.is_in_group("jump"):
+		velocity.y = jumpV
+		print("frick")
+		set_collision_mask_bit(2, false)
+	if area.is_in_group("floor"):
+		velocity.y = 0
+		set_collision_mask_bit(2, true)
+	pass # replace with function body
+
+
+#func _on_JumpBox_area_exited(area):
+#	if area.is_in_group("floor"):
+#		velocity.y = gravity
+#		set_collision_mask_bit(2, false)
+#	rando = randi()%2
+#	pass # replace with function body
--- World.gd
+++ World.gd
@@ -6,7 +6,10 @@ func _ready():
 	$AudioStreamPlayer2D.play()
 	pass
 
-#func _process(delta):
+func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-#	pass
+	if(Input.is_action_just_pressed("ui_cancel")):
+		get_tree().change_scene("res://MainMenu.tscn")
+	pass
+	
